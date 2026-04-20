605fc1724eceef0caca65882d5eb380b5fb879cd
Smoki <duducrin89@gmail.com>
2026-04-20 21:13:41 +0300

    Add server description and improve formatting

--- server.py
+++ server.py
@@ -320,4 +320,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
